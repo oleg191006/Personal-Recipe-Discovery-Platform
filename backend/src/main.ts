e7ca7855d2f0204e020267d5c858089b8426f50c
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-10-04 02:56:20 +0300

    feat: enhance application setup with cookie parser, CORS configuration, and global validation pipes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: process.env.CLIENT_URL ?? 'http://localhost:5173/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(3000);
 }
 bootstrap();
