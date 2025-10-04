516b601fc1543526792a2fe5bb33a1251e5d8d43
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-10-04 03:03:36 +0300

    refactor: simplify JwtStrategy constructor by removing unused UsersService dependency

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
-import { UsersService } from '../users/users.service';
 import { Request } from 'express';
 import { AuthUser } from 'src/shared/types/user-request.interface';
 
@@ -25,10 +24,7 @@ const extractJwtFromCookie = (req: Request): string | null => {
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    private readonly usersService: UsersService,
-    private readonly configService: ConfigService,
-  ) {
+  constructor(private readonly configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([extractJwtFromCookie]),
       ignoreExpiration: false,
